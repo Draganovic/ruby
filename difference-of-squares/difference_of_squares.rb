102cf35a23d07c4ca275ceacf1fe17f5be62ca7b
Admir Draganovic <admir.draganovic@gmail.com>
2016-05-07 15:43:02 -0600

    Add difference of squares to repo

---
+++ difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,24 @@
+require 'pry'
+class Squares
+
+  VERSION = 1
+
+  attr_reader :numbers
+
+  def initialize(numbers)
+    @numbers = (1..numbers)
+  end
+
+  def square_of_sums
+    numbers.reduce(0, :+)**2
+  end
+
+  def sum_of_squares
+    numbers.reduce(0) {|x, y| x + y**2}
+  end
+
+  def difference
+    square_of_sums - sum_of_squares
+  end
+
+end
