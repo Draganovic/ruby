efbc7af3f0ca5c71fa37a8df21130f3d83a71d91
Admir Draganovic <admir.draganovic@gmail.com>
2016-09-13 00:13:53 -0700

    Refactor

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,17 +1,15 @@
 class Hamming
-  VERSION = 1
-
+VERSION = 1
   def self.compute(s1, s2)
-
-    if s1.length != s2.length
+    unless s1.length == s2.length
       raise ArgumentError
     else
-
-    diff = s1.each_char.with_index.reduce(0) { |sum, (l, i)|
+    s1.each_char.with_index.reduce(0) { | sum, (l, i)|
       if l != s2[i]
         sum += 1
       end
-    sum }
+      sum
+    }
   end
 end
 
